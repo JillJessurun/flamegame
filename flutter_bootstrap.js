e9e058b2fdd9e618fedd848240f91c1aa754042b
JillJessurun <jilljessurun@gmail.com>
2025-02-24 14:11:31 +0100

    Built web
    Branch: master
    Commit: fedbe12a5ed13765c4fb64a3905ee3d2acdd4e2f (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1539319394"
+    serviceWorkerVersion: "778898796"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -44,14 +44,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "0c210cb0cde3bcd2c1d513c6ca5f91a4
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "c8a90486d31a92a6145ca4294f6f6fb4",
+"flutter_bootstrap.js": "a586b8dedc9dc0f4c848538697556537",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "226b8e3a2f7ba5c0d7d690d2eeb6e29c",
 "/": "226b8e3a2f7ba5c0d7d690d2eeb6e29c",
-"main.dart.js": "9db6ad0fa1ccba4ce951eb03805d0aaa",
+"main.dart.js": "dfad5c202a056677767da769d63cfd60",
 "manifest.json": "587c22c7f9d6afc83f04255e35f11d21",
 "version.json": "dbae49d8ad38b5205c67b01d190ff49e"};
 // The application shell files that are downloaded before a service worker can
